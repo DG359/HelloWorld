0c265fdd3615f2ff470589be8e87700b735f9f37
DG359 <daveg359@gmail.com>
2022-12-01 14:24:55 +0000

    First Commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication application = new SpringApplication(DemoApplication.class);
+		
+		application.setDefaultProperties(Collections.singletonMap("server.port", "8888"));
+		application.run(args);
+		
+		System.out.println("<<< App Started >>>>");
+	}
+
+}
+ 
---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello Spring Boot";
+		
+	}
+	
+	@RequestMapping("/")
+	public String sayRoot() {
+		return "Web app root";
+	}
+
+}
